afc56a98834c9468882a6237488f22ac1f7c7a80
Petter <1263775+petterhoel@users.noreply.github.com>
2020-02-29 20:54:30 +0100

    adds global confirmation for choco

--- windows/setup.bat
+++ windows/setup.bat
@@ -8,7 +8,8 @@
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-
+:: set global confirmation
+choco feature enable -n allowGlobalConfirmation
 
 :: install apps
 choco install vlc
