e10a4f4aa4d18f14f044e84e247c3c0383982f96
Petter <petter@petterhoel.net>
2023-02-12 13:28:43 +0100

    Remember to create dir on curl

--- mac/terminal-environment.sh
+++ mac/terminal-environment.sh
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/petterhoel/setup/main/posh/petter.omp.json -o ~/.omp/petter.omp.json
+curl https://raw.githubusercontent.com/petterhoel/setup/main/posh/petter.omp.json -o ~/.omp/petter.omp.json --create-dirs
 curl https://raw.githubusercontent.com/petterhoel/setup/main/.zshrc-append >> ~/.zshrc
