5191bb25468aa62ef812ba41c0bf28d83e7997d0
Petter <petter@petterhoel.net>
2023-02-16 07:27:50 +0100

    prefer silent winget installs

--- windows/installs.bat
+++ windows/installs.bat
@@ -1,21 +1,21 @@
 :: powershell core ? 
-winget install -e --id AgileBits.1Password
-winget install -e --id AgileBits.1Password.CLI
-winget install -e --id Axosoft.GitKraken
-winget install -e --id Git.Git
-winget install -e --id JanDeDobbeleer.OhMyPosh
-winget install -e --id JetBrains.Toolbox
-winget install -e --id LINQPad.LINQPad.7
-winget install -e --id Microsoft.AzureDataStudio
-winget install -e --id Mozilla.Firefox.DeveloperEdition
-winget install -e --id SlackTechnologies.Slack
-winget install -e --id TechSmith.Snagit.2023
-winget install -e --id XP9CSRSZ9PS7X0
-winget install -e --id Microsoft.Teams
-winget install -e --id Notion.Notion
-winget install -e --id Docker.DockerDesktop
-winget install -e --id CoreyButler.NVMforWindows 
-winget install -e --id Microsoft.PowerToys
+winget install --silent -e --id AgileBits.1Password
+winget install --silent -e --id AgileBits.1Password.CLI
+winget install --silent -e --id Axosoft.GitKraken
+winget install --silent -e --id Git.Git
+winget install --silent -e --id JanDeDobbeleer.OhMyPosh
+winget install --silent -e --id JetBrains.Toolbox
+winget install --silent -e --id LINQPad.LINQPad.7
+winget install --silent -e --id Microsoft.AzureDataStudio
+winget install --silent -e --id Mozilla.Firefox.DeveloperEdition
+winget install --silent -e --id SlackTechnologies.Slack
+winget install --silent -e --id TechSmith.Snagit.2023
+winget install --silent -e --id XP9CSRSZ9PS7X0
+winget install --silent -e --id Microsoft.Teams
+winget install --silent -e --id Notion.Notion
+winget install --silent -e --id Docker.DockerDesktop
+winget install --silent -e --id CoreyButler.NVMforWindows 
+winget install --silent -e --id Microsoft.PowerToys
 
 oh-my-posh font install
 Install-Module cd-extras
