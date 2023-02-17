8e484e2b9c40f4d762ed313cba69039f4da7656a
Petter <1263775+petterhoel@users.noreply.github.com>
2023-02-17 07:53:39 +0100

    Moves unfinished stuff to new files

---
+++ windows/files-folders.bat
@@ -0,0 +1,8 @@
+:: make directory for code
+mkdir C:\Code
+cd C:\Code
+
+:: todo
+::  -wsl
+::  -terminal setup
+::  -poweshell core setup
---
+++ windows/powershell/setup-posh.bat
@@ -0,0 +1,2 @@
+oh-my-posh font install
+Install-Module cd-extras
