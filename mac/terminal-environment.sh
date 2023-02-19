23106d75dff1a83e5be8d87da386466c87a36bdd
Petter <1263775+petterhoel@users.noreply.github.com>
2023-02-19 09:29:51 +0100

    Delete terminal-environment.sh

--- mac/terminal-environment.sh
+++
@@ -1,2 +0,0 @@
-curl https://raw.githubusercontent.com/petterhoel/setup/main/oh-my-posh/petter.omp.json -o ~/.omp/petter.omp.json --create-dirs
-curl https://raw.githubusercontent.com/petterhoel/setup/main/mac/.zshrc-append >> ~/.zshrc
