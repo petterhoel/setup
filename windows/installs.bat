3580741d69d192d240e2c7c9d2ae0822f9e32252
Petter <1263775+petterhoel@users.noreply.github.com>
2023-12-06 16:17:23 +0100

    Adds github desktop to windows installs

--- windows/installs.bat
+++ windows/installs.bat
@@ -18,3 +18,4 @@ winget install --silent -e --id Docker.DockerDesktop
 winget install --silent -e --id CoreyButler.NVMforWindows 
 winget install --silent -e --id Microsoft.PowerToys
 winget install --silent -e --id GitHub.cli
+winget install --silent -e --id GitHub.GitHubDesktop
