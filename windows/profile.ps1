36dde4b429d0005658cba5ca795aef56a0854d81
Petter <1263775+petterhoel@users.noreply.github.com>
2024-06-12 08:45:22 +0200

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -5,6 +5,7 @@ oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/petterhoel/setu
 
 # navigering
 function pas { set-location "D:/source/Pasientrutine/Furst.Pasientrutine.Frontend" }
+function fp { set-location "D:/source/PasientWeb/src/Furst.Pasient.Frontend" }
 function lm { set-location "D:/source/LisMikro/Furst.LisMikro.Frontend" }
 function pl { set-location "D:/source/Plattform/Lis" }
 function src { set-location "D:/source" }
@@ -25,3 +26,5 @@ function ga { git add "-A" }
 function gpu { git pull }
 function gcom ([String] $message) { git commit -m $message }
 function gb ([String] $branchname){ git checkout -b $branchname }
+
+src
