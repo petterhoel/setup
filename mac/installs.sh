b8f6bda7b28b0f82ca774139f86ab14d617f423c
Petter <1263775+petterhoel@users.noreply.github.com>
2024-11-29 13:09:48 +0100

    Oppdaterer installs.sh for mac

--- mac/installs.sh
+++ mac/installs.sh
@@ -1,36 +1,25 @@
 # install apps
-brew install --cask 1password
-brew install --cask keeper-password-manager
-brew install --cask bettertouchtool
-brew install --cask chrysalis
-brew install --cask firefox@developer-edition
-brew install --cask gitkraken
-brew install --cask iterm2
-brew install --cask jetbrains-toolbox
-brew tap microsoft/git
-brew install --cask git-credential-manager-core
-brew install gh
-brew install --cask github
-brew install --cask microsoft-edge
-brew install --cask microsoft-remote-desktop
-brew install --cask microsoft-teams
-brew install --cask notion
-brew install --cask qobuz
-brew install --cask raycast
-brew install --cask rocket
-brew install --cask slack
-brew install --cask snagit
-brew install --cask vlc
-brew install --cask alt-tab
+brew install azure-cli
 brew install docker
 brew install docker-compose
-brew install azure-cli
-brew install --cask polypane
-
-## oh my posh
-brew install jandedobbeleer/oh-my-posh/oh-my-posh
-oh-my-posh font install
-
-#nvm && node
+brew install gh
 brew install nvm
 
+brew install --cask 1password
+brew install --cask dotnet-sdk
+brew install --cask eloston-chromium
+brew install --cask firefox@developer-edition
+brew install --cask git-credential-manager-core
+brew install --cask github
+brew install --cask gitkraken
+brew install --cask jetbrains-toolbox
+brew install --cask karabiner-elements
+brew install --cask keeper-password-manager
+brew install --cask microsoft-teams
+brew install --cask obsidian
+brew install --cask polypane
+brew install --cask qobuz
+brew install --cask raycast
+brew install --cask slack
+brew install --cask snagit
+brew install --cask windows-app
